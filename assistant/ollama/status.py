7d1fb6a1bba87aa3484d9acaa27ba6d66433c88e
Stephen Grice <stephengrice@live.com>
2026-02-16 05:23:50 -0500

    fix: check if result.stdout

--- assistant/ollama/status.py
+++ assistant/ollama/status.py
@@ -6,7 +6,7 @@ container_name = hex.get_state("container_name")
 result = subprocess.run(f"podman ps --filter name={container_name} -q".split())
 
 if result.returncode == 0:
-    if len(result.stdout.strip()) > 0:
+    if result.stdout and len(result.stdout.strip()) > 0:
         print("Running.")
     else:
         print("Not running.")
