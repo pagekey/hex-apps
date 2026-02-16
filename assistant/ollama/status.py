d1a8477cf2eecd24385ab5af354c4d25fc481df4
Stephen Grice <stephengrice@live.com>
2026-02-16 05:34:07 -0500

    fix: capture output for status

--- assistant/ollama/status.py
+++ assistant/ollama/status.py
@@ -3,10 +3,14 @@ import subprocess
 
 container_name = hex.get_state("container_name")
 
-result = subprocess.run(f"podman ps --filter name={container_name} -q".split())
+result = subprocess.run(
+    f"podman ps --filter name={container_name} -q".split(),
+    text=True,
+    capture_output=True,
+)
 
 if result.returncode == 0:
-    if result.stdout and len(result.stdout.strip()) > 0:
+    if len(result.stdout.strip()) > 0:
         print("Running.")
     else:
         print("Not running.")
