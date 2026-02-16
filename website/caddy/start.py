80064df8815ec44024a814ee87024b42df7d6887
Stephen Grice <stephengrice@live.com>
2026-02-16 05:46:19 -0500

    fix: add --browse to caddy start

--- website/caddy/start.py
+++ website/caddy/start.py
@@ -7,6 +7,6 @@ port_map = hex.get_state("port_map")
 image = hex.get_state("image")
 path = Path(hex.get_state("path")).absolute()
 
-cmd = f"podman run -d -v {path}:/srv --name {container_name} -p {port_map} {image} caddy file-server --root /srv --listen :80"
+cmd = f"podman run -d -v {path}:/srv --name {container_name} -p {port_map} {image} caddy file-server --root /srv --listen :80 --browse"
 
 subprocess.check_call(cmd.split())
