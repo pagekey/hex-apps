8c6ec9f933d95f42b5c33e9d3b835793f9a1f425
Stephen Grice <stephengrice@live.com>
2026-02-16 05:28:37 -0500

    fix: swap start/stop scripts

--- assistant/ollama/stop.py
+++ assistant/ollama/stop.py
@@ -2,9 +2,6 @@ import hex
 import subprocess
 
 container_name = hex.get_state("container_name")
-port_map = hex.get_state("port_map")
-image = hex.get_state("image")
 
-cmd = f"podman run -d --name {container_name} -p {port_map} {image}"
-
-subprocess.check_call(cmd.split())
+subprocess.check_call(f"podman stop {container_name}".split())
+subprocess.check_call(f"podman rm {container_name}".split())
