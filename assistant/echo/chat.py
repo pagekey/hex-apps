89d63afda35170c50a25cc50d5336e471f6e9265
Stephen Grice <stephengrice@live.com>
2026-02-17 07:30:19 -0500

    fix: correct echo assistant chat, delete svc

--- assistant/echo/chat.py
+++ assistant/echo/chat.py
@@ -1,3 +1,8 @@
 import hex
 
-print(f"AI ({hex.state()['model_name']}): You said {hex.inputs()['prompt']}")
+# Gather state and inputs.
+model_name = hex.get_state("model_name")
+prompt = hex.get_input("prompt")
+
+# Print the response
+print(f"AI ({model_name}): You said {prompt}")
